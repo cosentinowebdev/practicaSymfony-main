341bdfd9a91b6a5a7fd57a6063f919ab92790b6f
cosentinowevdev <rodrigocosentino@outlook.com>
2021-04-02 03:21:32 -0300

    buscar por categoria

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriaRepository;
 use App\Repository\MarcadorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,10 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
-    public function index(MarcadorRepository $marcadorRepository): Response
+
+    #[Route('/{categoria}', name: 'app_index', defaults: ['categoria'=> ''])]
+    public function index(string $categoria, CategoriaRepository $categoriaRepository,MarcadorRepository $marcadorRepository): Response
     {
-        $marcadores = $marcadorRepository->findAll();
+        dump($categoria);
+        if (!empty($categoria))
+        {
+            if(!$categoriaRepository->findByNombre($categoria))
+            {
+                throw $this->createNotFoundException("La categoria '$categoria' no existe.");
+            }
+            $marcadores = $marcadorRepository->buscarPorNombreCategoria($categoria);
+
+        } else {
+            $marcadores = $marcadorRepository->findAll();
+
+        }
+
         return $this->render('index/index.html.twig', [
             'marcadores' => $marcadores,
         ]);
