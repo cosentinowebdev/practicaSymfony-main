8e7b0b67f129543ada3f981c58adc46ee767b0fc
cosentinowevdev <rodrigocosentino@outlook.com>
2021-04-04 02:05:48 -0300

    icono marcadores favoritos

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,13 +6,36 @@ use App\Repository\CategoriaRepository;
 use App\Repository\MarcadorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+
+    #[Route('/editar-favorito', name: 'app_editar_favorito')]
+    public function editarFavorito(MarcadorRepository $marcadorRepository,Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $actualizado = true;
+            $idMarcador = $request-> get('id');
+            $entityManager = $this->getDoctrine()->getManager();
+            $marcador = $marcadorRepository->findOneById($idMarcador);
+            $marcador->setFavorito(!$marcador->getFavorito());
+            try {
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                $actualizado = false;
+            }
+            return $this->json([
+                'actualizado' => $actualizado
+            ]);
+        }   
+        throw $this->createNotFoundException();
+    }
+
     #[Route('/favoritos', name: 'app_favoritos')]
     public function favoritos(MarcadorRepository $marcadorRepository){
-        $marcadores = $marcadorRepository->find([
+        $marcadores = $marcadorRepository->findBy([
             'favorito' => true
         ]);
         return $this->render('index/index.html.twig', [
